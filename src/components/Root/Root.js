33e072055a9bf46b0222f9e889a47b8cd5a5c1cc
Grzegorz <domaradzki.g@gmail.com>
2019-03-20 19:36:48 +0100

    added layout login component

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from '../Dashboard'
+import Login from '../Login'
 
 class Root extends Component {
   render() {
     return (
+      <div><Login />
       <Router>
         <Route exact path="/dashboard" component={Dashboard} />
       </Router>
+      </div>
     );
   }
 }
