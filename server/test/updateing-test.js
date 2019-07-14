fb45e5e636d2e214747f1465b9113dfd9ed85452
Grzegorz <domaradzki.g@gmail.com>
2019-07-14 11:47:38 +0200

    added second test to update with incrementation

--- server/test/updateing-test.js
+++ server/test/updateing-test.js
@@ -4,7 +4,8 @@ describe("Updateing records", function() {
   var newOrder;
   this.beforeEach(function(done) {
     newOrder = new Order({
-      assortment: "Taśma z nadrukiem"
+      assortment: "Taśma z nadrukiem",
+      quantity: 144
     });
     newOrder.save().then(function() {
       done();
@@ -18,6 +19,15 @@ describe("Updateing records", function() {
     ).then(function() {
       Order.findOne({ _id: newOrder._id }).then(function(result) {
         assert(result.assortment === "Taśma pakowa");
+        done();
+      });
+    });
+  });
+
+  it("Increment quantity record by 36 in the database", function(done) {
+    Order.updateMany({}, { $inc: { quantity: 36 } }).then(function() {
+      Order.findOne({ assortment: "Taśma z nadrukiem" }).then(function(record) {
+        assert(record.quantity === 180);
         done();
       });
     });
