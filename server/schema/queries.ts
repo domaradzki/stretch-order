2eda6bfeb4dbab2607a031ddf1f2d62a27a37c6a
Grzegorz <domaradzki.g@gmail.com>
2019-08-21 11:39:40 +0200

    added  queries for Tapes and Stretches

--- server/schema/queries.ts
+++ server/schema/queries.ts
@@ -1,12 +1,20 @@
 import { GraphQLObjectType } from "graphql";
 import documentQueries from "./documents/queries";
 import orderQueries from "./orders/queries";
+import userQueries from "./users/queries";
+import clientQueries from "./clients/queries";
+import tapeQueries from "./tapes/queries";
+import stretchQueries from "./stretches/queries";
 
 const query = new GraphQLObjectType({
   name: "Query",
   fields: {
     ...documentQueries,
-    ...orderQueries
+    ...orderQueries,
+    ...userQueries,
+    ...clientQueries,
+    ...tapeQueries,
+    ...stretchQueries
   }
 });
 
