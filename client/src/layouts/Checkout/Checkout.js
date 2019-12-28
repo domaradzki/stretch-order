6861df9efc238d82d2a2edb91e07810e846bb3ed
Grzegorz <domaradzki.g@gmail.com>
2019-12-28 19:49:27 +0100

    funcionality with adding the orders moved to the new component, things working as before with semantic, app have a nice look

--- client/src/layouts/Checkout/Checkout.js
+++ client/src/layouts/Checkout/Checkout.js
@@ -16,8 +16,10 @@ import addOrderMutation from "../../graphql/mutations/addOrderMutation";
 import addDocumentMutation from "../../graphql/mutations/addDocumentMutation";
 import addClientMutation from "../../graphql/mutations/addClientMutation";
 import addUserMutation from "../../graphql/mutations/addUserMutation";
+import addTapeMutation from "../../graphql/mutations/addTapeMutation";
+import addStretchMutation from "../../graphql/mutations/addStretchMutation";
 import isInDatabase from "../../graphql/queries/isInDatabase";
-// import getOrdersItemid from "../../graphql/queries/getOrdersItemid";
+import getOrdersItemid from "../../graphql/queries/getOrdersItemid";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -103,8 +105,183 @@ function Checkout(props) {
 
   const handleAddOrder = event => {
     event.preventDefault();
-    console.log(activeOrder);
-    console.log(input);
+    const data = Object.assign(activeOrder, input);
+    const {
+      client,
+      quantity,
+      price,
+      netValue,
+      details,
+      dateInsert,
+      invoice,
+      dateOfPay,
+      dateOfRealisation,
+      deliveryAddress,
+      trader,
+      transport,
+      assortment,
+      signature,
+      documentStatus,
+      closed,
+      symbol,
+      itemId,
+      code,
+      kind,
+      type,
+      unit,
+      currency,
+      exchangeRate,
+      numberOfDocumentInvoice,
+      companyId,
+      documentId,
+      sleeve,
+      stretchColor,
+      stretchThickness,
+      netWeight,
+      grossWeight,
+      printName,
+      tapeLong,
+      tapeWidth,
+      tapeThickness,
+      tapeColor,
+      numberOfColors,
+      glue,
+      roller,
+      color1,
+      color2,
+      color3,
+      dateOfAcceptation
+    } = data;
+    if (!props.data.isLoading) {
+      const isClient = props.data.client;
+      const isUser = props.data.user;
+      const isDocument = props.data.document;
+      const isTapeProduct = kind === "KT" && type === "TPD";
+      const isStretchProduct = kind === "KT" && type === "FS";
+      const addingClient = () =>
+        props
+          .addClientMutation({
+            variables: {
+              name: client,
+              companyId
+            }
+          })
+          .then(res => res.data.addClient.id);
+
+      const addingUser = () =>
+        props
+          .addUserMutation({
+            variables: {
+              name: trader
+            }
+          })
+          .then(res => res.data.addUser.id);
+
+      const addingTape = async () =>
+        await props
+          .addTapeMutation({
+            variables: {
+              printName,
+              dateOfAcceptation,
+              numberOfColors,
+              color1,
+              color2,
+              color3,
+              glue,
+              roller,
+              tapeColor,
+              tapeLong,
+              tapeThickness,
+              tapeWidth
+            }
+          })
+          .then(res => res.data.addTape.id);
+
+      const addingStretch = async () =>
+        await props
+          .addStretchMutation({
+            variables: {
+              sleeve,
+              stretchColor,
+              stretchThickness,
+              netWeight,
+              grossWeight
+            }
+          })
+          .then(res => res.data.addStretch.id);
+
+      const addingOrder = async (idDoc, idProduct) =>
+        await props.addOrderMutation({
+          variables: {
+            itemId,
+            name: assortment,
+            code,
+            kind,
+            type,
+            quantity,
+            unit,
+            price,
+            netValue,
+            documentId: idDoc,
+            productId: idProduct
+          },
+          refetchQueries: [{ query: getOrdersItemid }]
+        });
+
+      const addingDocument = (idC, idU) =>
+        props
+          .addDocumentMutation({
+            variables: {
+              documentId,
+              dateInsert,
+              dateOfPay,
+              dateOfRealisation,
+              signature,
+              symbol,
+              details,
+              closed,
+              currency,
+              exchangeRate,
+              documentStatus,
+              deliveryAddress,
+              transport,
+              numberOfDocumentInvoice,
+              invoice,
+              clientId: idC,
+              userId: idU
+            }
+          })
+          .then(res => res.data.addDocument.id);
+
+      const promiseIfNoClient = async () =>
+        isClient ? isClient.id : await addingClient();
+
+      const promiseIfNoUser = async () =>
+        isUser ? isUser.id : await addingUser();
+
+      const promiseIfNoDocument = async (idC, idU) =>
+        isDocument ? isDocument.id : await addingDocument(idC, idU);
+
+      const addingProduct = isTapeProduct
+        ? addingTape()
+        : isStretchProduct
+        ? addingStretch()
+        : null;
+
+      Promise.all([promiseIfNoClient(), promiseIfNoUser(), addingProduct])
+        .then(result => {
+          return {
+            clientId: result[0],
+            userId: result[1],
+            productId: result[2]
+          };
+        })
+        .then(res => {
+          promiseIfNoDocument(res.clientId, res.userId).then(r => {
+            addingOrder(r, res.productId);
+          });
+        });
+    }
     setActiveStep(activeStep + 1);
   };
 
@@ -163,6 +340,10 @@ const graphqlClient = graphql(addClientMutation, {
 const graphqlUser = graphql(addUserMutation, {
   name: "addUserMutation"
 });
+const graphqlTape = graphql(addTapeMutation, { name: "addTapeMutation" });
+const graphqlStretch = graphql(addStretchMutation, {
+  name: "addStretchMutation"
+});
 
 const graphqlCheck = graphql(isInDatabase, {
   options: props => {
@@ -181,5 +362,7 @@ export default compose(
   graphqlDocument,
   graphqlClient,
   graphqlUser,
+  graphqlTape,
+  graphqlStretch,
   graphqlCheck
 )(Checkout);
