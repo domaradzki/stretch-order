e3fb43f4b2d7ad1f04dc676bcdf1b297a5d6fde5
Grzegorz <domaradzki.g@gmail.com>
2020-01-15 23:26:01 +0100

    add file to mongoDB, conection and transfer ok - WIP

--- server/schema/files/mutations.ts
+++ server/schema/files/mutations.ts
@@ -1,10 +1,14 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as mongoose from "mongoose";
+
 import { GraphQLString, GraphQLNonNull } from "graphql";
 import { GraphQLUpload } from "graphql-upload";
+
 import FileType from "./fileType";
 import File from "../../models/file";
 import { FileInterface } from "../../types/fileType";
+import { connectMongoDB } from "../../connection";
 
 const fileMutations = {
   singleUpload: {
@@ -28,6 +32,14 @@ const fileMutations = {
       }
       const filestream = await createReadStream();
       filestream.pipe(fs.createWriteStream(`./uploadFiles/${filename}`));
+      const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
+      const uploadStream = bucket.openUploadStream(filename);
+      await new Promise((resolve, reject) => {
+        filestream
+          .pipe(uploadStream)
+          .on("error", reject)
+          .on("finish", resolve);
+      });
       return args.file;
     }
   }
